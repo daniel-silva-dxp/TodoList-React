bd8b00c73ff05bed2255a2242b3ea0fcbc618a9a
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-20 12:51:43 -0300

    feat: importando Main.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Main from './components/main';
 
 function App() {
-	return <div className="App">Daniel Silva</div>;
+	return (
+		<div>
+			<Main />
+		</div>
+	);
 }
 
 export default App;
