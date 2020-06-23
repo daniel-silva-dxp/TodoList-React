ed2e68b25bbb33fe863d38b9ac9d19b35a93370a
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-22 21:38:01 -0300

    feat: organizando as páginas em pasta para trabalhar com rotas

---
+++ src/pages/home/index.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Header from '../../components/header';
+import Form from '../..//components/form';
+import TodoList from '../..//components/todo-list';
+import Footer from '../..//components/footer';
+
+class Home extends Component {
+	constructor() {
+		super()
+		this.state = {
+			task: '',
+			list: [],
+			index: -1
+		}
+		this.addTask = this.addTask.bind(this);
+		this.editTask = this.editTask.bind(this);
+		this.deleteTask = this.deleteTask.bind(this);
+	}
+
+	componentDidMount() {
+		const list = JSON.parse(localStorage.getItem('list'));
+
+		if(!list) return 
+
+		this.setState({ list })
+	}
+	componentDidUpdate(prevProps, prevState) {
+		const { list } = this.state
+		if(list === prevState.list) return 
+
+		localStorage.setItem('list', JSON.stringify(list));
+	}
+
+	addTask(e) {
+		e.preventDefault();
+		
+		const { list, index } = this.state;
+		let { task } = this.state
+
+		if(list.indexOf(task) !== -1) return;
+		
+		const newList = [...list];
+		if(index === -1) {
+			this.setState({
+				list: [...newList, task],
+				task: ''
+			})
+		} else {
+			newList[index] = task;
+			this.setState({
+				list: [...newList],
+				index: -1
+			})
+		}
+	}
+	editTask(e, index) {
+		const { list } = this.state;
+		this.setState({
+			index,
+			task: list[index]
+		})
+	}
+	deleteTask(e, index) {
+		const { list } = this.state;
+		const newList = [...list];
+		newList.splice(index, 1);
+		this.setState({list: [...newList]});
+	}
+	render() {
+		return (
+			<div>
+				<Header />
+				<div className="container">
+					<Form
+						handleSubmit={this.addTask} 
+						handleChange={(e) => this.setState({task: e.target.value})} 
+						task={this.state.task}	
+						placeholder="Your Task" 
+					/>
+                    <TodoList list={this.state.list} editTask={this.editTask} deleteTask={this.deleteTask}/>
+					<Footer />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
