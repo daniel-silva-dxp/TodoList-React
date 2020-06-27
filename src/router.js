8cf17d9c100330cf8e26052671c63787110c360c
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-27 13:33:38 -0300

    feat: importando page erro à rotas

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Navbar from './components/navbar';
+import PgErro from './pages/erro';
 
 const Routes = () => {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Switch>
 				<Route exact path="/" component={Home} />
+				<Route exact path="/status" component={PgErro} />>
 			</Switch>
 		</BrowserRouter>
 	);
