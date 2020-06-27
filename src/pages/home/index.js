e184888839a9bd339478139461f6e6a176c30ad2
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-27 13:35:34 -0300

    feat: alterando estilo da page home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,12 +6,12 @@ import Footer from '../..//components/footer';
 
 class Home extends Component {
 	constructor() {
-		super()
+		super();
 		this.state = {
 			task: '',
 			list: [],
 			index: -1
-		}
+		};
 		this.addTask = this.addTask.bind(this);
 		this.editTask = this.editTask.bind(this);
 		this.deleteTask = this.deleteTask.bind(this);
@@ -20,37 +20,37 @@ class Home extends Component {
 	componentDidMount() {
 		const list = JSON.parse(localStorage.getItem('list'));
 
-		if(!list) return 
+		if (!list) return;
 
-		this.setState({ list })
+		this.setState({ list });
 	}
 	componentDidUpdate(prevProps, prevState) {
-		const { list } = this.state
-		if(list === prevState.list) return 
+		const { list } = this.state;
+		if (list === prevState.list) return;
 
 		localStorage.setItem('list', JSON.stringify(list));
 	}
 
 	addTask(e) {
 		e.preventDefault();
-		
-		const { list, index } = this.state;
-		let { task } = this.state
 
-		if(list.indexOf(task) !== -1) return;
-		
-		const newList = [...list];
-		if(index === -1) {
+		const { list, index } = this.state;
+		let { task } = this.state;
+
+		if (list.indexOf(task) !== -1) return;
+
+		const newList = [ ...list ];
+		if (index === -1) {
 			this.setState({
-				list: [...newList, task],
+				list: [ ...newList, task ],
 				task: ''
-			})
+			});
 		} else {
 			newList[index] = task;
 			this.setState({
-				list: [...newList],
+				list: [ ...newList ],
 				index: -1
-			})
+			});
 		}
 	}
 	editTask(e, index) {
@@ -58,28 +58,30 @@ class Home extends Component {
 		this.setState({
 			index,
 			task: list[index]
-		})
+		});
 	}
 	deleteTask(e, index) {
 		const { list } = this.state;
-		const newList = [...list];
+		const newList = [ ...list ];
 		newList.splice(index, 1);
-		this.setState({list: [...newList]});
+		this.setState({ list: [ ...newList ] });
 	}
 	render() {
 		return (
 			<div>
-				<Header />
-				<div className="container">
-					<Form
-						handleSubmit={this.addTask} 
-						handleChange={(e) => this.setState({task: e.target.value})} 
-						task={this.state.task}	
-						placeholder="Your Task" 
-					/>
-                    <TodoList list={this.state.list} editTask={this.editTask} deleteTask={this.deleteTask}/>
-					<Footer />
+				<div className="main">
+					<Header />
+					<div className="container">
+						<Form
+							handleSubmit={this.addTask}
+							handleChange={(e) => this.setState({ task: e.target.value })}
+							task={this.state.task}
+							placeholder="Your Task"
+						/>
+						<TodoList list={this.state.list} editTask={this.editTask} deleteTask={this.deleteTask} />
+					</div>
 				</div>
+				<Footer />
 			</div>
 		);
 	}
