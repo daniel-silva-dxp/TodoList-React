5c207f3c04ee1221e58a3217fc5695db09424978
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-27 13:31:21 -0300

    feat: criando e estilizando component construct

---
+++ src/components/construct/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import construct from '../../assets/construction.svg';
+
+const ConstructImg = () => {
+	return (
+		<div>
+			<img src={construct} alt="" />
+		</div>
+	);
+};
+
+export default ConstructImg;
